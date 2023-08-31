6199d3ebd512206972e4ed9a97e9d7e9fc9c8c89
Diego Diaz <rdiazmorfin@gmail.com>
2023-08-31 16:58:07 -0600

    get y post

--- U1/server.py
+++ U1/server.py
@@ -1,6 +1,8 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 
+contador = 11
+
 class MyHTTPRequestHandler(BaseHTTPRequestHandler):
     def _set_response(self, content_type="text/plain"):
         self.send_response(200)
@@ -9,11 +11,22 @@ class MyHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self._set_response()
-        self.wfile.write("Hello from the server!".encode())
+        respuesta = "el valor es: " + str(contador)
+        self.wfile.write(respuesta.encode())
 
     def do_POST(self):
         content_length = int(self.headers["Content-Length"])
         post_data = self.rfile.read(content_length)
+        
+        body_json = json.loads(post_data.decode())
+        
+        
+        global contador
+        
+        if(body_json['action'] == 'asc'):
+            contador += 1
+        elif(body_json['action'] == 'desc'):
+            contador -= 1
 
         # Print the complete HTTP request
         print("\n----- Incoming POST Request -----")
@@ -23,7 +36,8 @@ class MyHTTPRequestHandler(BaseHTTPRequestHandler):
         print("-------------------------------")
 
         # Respond to the client
-        response_data = json.dumps({"message": "Received POST data", "data": post_data.decode()})
+        response_data = json.dumps({"message": "Received POST data", 
+                                    "data": post_data.decode()})
         self._set_response("application/json")
         self.wfile.write(response_data.encode())
 
