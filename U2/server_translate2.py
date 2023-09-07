89c86bfbb8176ed40dd3c23061ae41776d143cf0
Diego Diaz <rdiazmorfin@gmail.com>
2023-09-07 16:40:26 -0600

    ejercicio hecho en clase

---
+++ U2/server_translate2.py
@@ -0,0 +1,74 @@
+# Importa las clases necesarias del módulo http.server y json
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+import requests
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+X_RAPIDAPI_KEY = os.getenv("X_RAPIDAPI_KEY")
+# Inicializa un contador en 0
+contador = 0
+
+# Define una clase personalizada que hereda de BaseHTTPRequestHandler
+
+
+class MyHTTPRequestHandler(BaseHTTPRequestHandler):
+    def _set_response(self, content_type="text/plain", status_code=200):
+        # Configura la respuesta HTTP con el código 200 (OK) y el tipo de contenido especificado
+        self.send_response(status_code)
+        self.send_header("Content-type", content_type)
+        self.end_headers()
+        
+    def throw_custom_error(self, message, status_code = 400 ):
+        self._set_response("application/json", status_code)
+        self.wfile.write(json.dumps({"message": message}).encode())
+
+    def do_GET(self):
+        # Maneja las solicitudes GET
+        self._set_response()
+        respuesta = "el valor es: " + str(contador)
+        self.wfile.write(respuesta.encode())
+
+    def do_POST(self):
+        content_length = int(self.headers["Content-Length"])
+        post_data = self.rfile.read(content_length)
+
+        try:
+            body_json = json.loads(post_data.decode())
+        except:
+            self.throw_custom_error("Invalid JSON")
+            return
+
+        global contador
+        
+        url = "https://google-translate1.p.rapidapi.com/language/translate/v2"
+        payload = {"q": "Hello, furry world!", "target": "es", "source": "en"}
+        headers = {"content-type": "application/x-www-form-urlencoded", "Accept-Encoding": "application/gzip", "X-RapidAPI-Key": X_RAPIDAPI_KEY, "X-RapidAPI-Host": "google-translate1.p.rapidapi.com"}
+        response = requests.post(url, data=payload, headers=headers)
+        
+        texto_traducido = response.json()["data"]["translations"][0]["translatedText"]
+        
+    
+        print("\n----- Incoming POST Request -----")
+        print(f"Requestline: {self.requestline}")
+        print(f"Headers:\n{self.headers}")
+        print(f"Body:\n{post_data.decode()}")
+        print("-------------------------------")
+
+        response_data = json.dumps({"TransladeText": texto_traducido})
+        self._set_response("application/json")
+        self.wfile.write(response_data.encode())
+
+
+def run_server(server_class=HTTPServer, handler_class=MyHTTPRequestHandler, port=7800):
+    server_address = ("", port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Starting server on port {port}...")
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run_server()
